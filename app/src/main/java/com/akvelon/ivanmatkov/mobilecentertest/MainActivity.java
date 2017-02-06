a6f2a59399f6c2997180bbf7e37be033d77deda1
Ivan Matkov <matkov.ivan@gmail.com>
2017-02-06 15:24:07 +0300

    Added test buttons.

--- app/src/main/java/com/akvelon/ivanmatkov/mobilecentertest/MainActivity.java
+++ app/src/main/java/com/akvelon/ivanmatkov/mobilecentertest/MainActivity.java
@@ -7,6 +7,9 @@ import com.microsoft.azure.mobile.MobileCenter;
 import com.microsoft.azure.mobile.analytics.Analytics;
 import com.microsoft.azure.mobile.crashes.Crashes;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String MOBILE_CENTER_APP_ID = "4250a53c-8bf1-4057-9bf5-32e4179ccd33";
@@ -17,5 +20,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         MobileCenter.start(getApplication(), MOBILE_CENTER_APP_ID, Analytics.class, Crashes.class);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.send_event_button)
+    public void sendEvent() {
+        Analytics.trackEvent("Test event");
+    }
+
+    @OnClick(R.id.test_crach_button)
+    public void testCrash() {
+        Crashes.generateTestCrash();
     }
 }
